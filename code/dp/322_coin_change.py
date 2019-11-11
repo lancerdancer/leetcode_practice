23b2c85f624c4edccda5ce3aaec1b2f7d63ab93e
lancerd <lanceryao@gmail.com>
2019-11-11 00:00:39 -0800

    coin change

---
+++ code/dp/322_coin_change.py
@@ -0,0 +1,36 @@
+"""
+https://leetcode.com/problems/coin-change/
+
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the
+fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any
+combination of the coins, return -1.
+
+Example 1:
+
+Input: coins = [1, 2, 5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Note:
+You may assume that you have an infinite number of each kind of coin.
+"""
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [sys.maxsize] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        if dp[-1] == sys.maxsize:
+            return -1
+        return dp[-1]
+
+
+
