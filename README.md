501951d422a68611e05c2eb6c2ae4c381c99e124
yongxu.yao <lanceryao@gmail.com>
2019-12-27 20:21:31 -0800

    Add a Boyer-Moore Voting Algorithm example

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Leetcode Practice
 Problems from leetcode/lintcode and my solutions for them.  
+
+## Specialized Algorithms 
+|  Name  | Specialization | Example |
+| ------ | -------------- | ------- |
+| Boyer-Moore Voting Algorithm | Find the majority of a sequence of elements using linear time and constant space. | [169 Majority Element](code/list/169_majority_element.py) |
---
+++ code/list/169_majority_element.py
@@ -0,0 +1,29 @@
+"""
+https://leetcode.com/problems/majority-element/
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+
+"""
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            count += (1 if num == candidate else -1)
+
+        return candidate
